c439a876e248a66267207083766bfe2e20bc61d7
Ihar Yazerski <ihar.yazerski@gmail.com>
2022-09-18 22:19:05 +0400

    v1.1:
    - update deploy scripts to support latest docker version;
    
    - bump docker python version to 3.10;
    
    - add .dockerignore;
    
    - implement support of lock-files for requirements;
    
    - update requirements;

--- setup.py
+++ setup.py
@@ -5,22 +5,22 @@ from pyrandseqbot import __version__
 with open('README.md', 'r', encoding='utf-8') as fp:
     long_description = fp.read()
 
-requirements = []
-for file in ['common', 'prod']:
-    with open(f'requirements/{file}.txt', encoding='utf-8') as fp:
-        for line in fp:
-            if line.startswith('git'):  # handle private repositories
-                requirements.append(f'{line.split("/")[-1].split(".git")[0]} @ {line}')
-            elif line.startswith('-'):
-                continue  # ignore other files including
-            else:
-                requirements.append(line)
+requirements = set()
+with open(f'requirements/prod.txt', encoding='utf-8') as fp:
+    for line in fp:
+        line = line.strip()
+        if line.startswith('git'):  # handle private repositories
+            requirements.add(f'{line.split("/")[-1].split(".git")[0]} @ {line}')
+        elif not line or line.startswith('-') or line.startswith('#'):
+            continue  # ignore other files including and comments
+        else:
+            requirements.add(line)
 
 setuptools.setup(
     name='pyrandseqbot',
     version=__version__,
     author='Ihar Yazerski',
-    author_email='ihar.yazerski@outlook.com',
+    author_email='ihar.yazerski@gmail.com',
     description='Telegram bot that creates random sequences',
     include_package_data=True,
     long_description=long_description,
